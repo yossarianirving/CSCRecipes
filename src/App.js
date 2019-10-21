caeaf69d818025682817c1537298e7d80247f361
Chris Ward <acward96@gmail.com>
2019-10-21 07:59:47 -0400

    Initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import {
+  BASE_URL,
+  API
+} from './constants'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      results: null
+    };
+    this.getRecipes = this.getRecipes.bind(this)
+  }
+
+  componentDidMount() {
+    this.getRecipes()
+  }
+
+  render() {
+    const { results } = this.state;
+    return(
+      results ?
+      <div>
+        {
+          results.map(recipe => 
+            <RecipeCard
+              title={recipe['title']}
+              field_images={recipe['field_images']}
+              body={recipe['body']}  ></RecipeCard>
+        )}
+      </div>
+        : <h1>Loading</h1>
+    )
+  }
+
+  getRecipes() {
+    fetch(`${BASE_URL}${API}/rec`, {
+      credentials: 'include',
+      // mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then(response => {
+      console.log(response);
+      
+      return response.json()  
+    }).then(results => {
+      this.setState({ results })
+    })
+  }
 }
+
+
+const RecipeCard = ({title, field_images, body}) => {
+  let img = field_images.split(',').slice(-1)[0];
+  return(
+    <div className="recipe-card">
+      <div className="recipe-img">
+        <img src={`${BASE_URL}${img}`}></img>
+      </div>
+      <div>
+        { title }
+      </div>
+    </div>
+  )
+}
+
 
 export default App;
---
+++ src/constants.js
@@ -0,0 +1,4 @@
+// export const BASE_URL = "http://gtest.dev.wwbtc.com";
+export const BASE_URL = "http://192.168.122.200"
+export const LOGIN = "/user/login?_format=json"
+export const API = "/json"
