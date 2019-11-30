d9a5aad5bac644afc945cee035efedbd879ea8d2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-30 20:34:43 +0000

    Deploy yossarianirving/CSCRecipes to github.com/yossarianirving/CSCRecipes.git:gh-pages

---
+++ precache-manifest.2a840c8a74ae86e3aaf2e4b48340d022.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf633cf6389579d2e088ea8342398cce",
+    "url": "/CSCRecipes/index.html"
+  },
+  {
+    "revision": "b33034107fe9441a2d8b",
+    "url": "/CSCRecipes/static/css/main.b10f0747.chunk.css"
+  },
+  {
+    "revision": "85c0c714b49132b9e2b6",
+    "url": "/CSCRecipes/static/js/2.c550971c.chunk.js"
+  },
+  {
+    "revision": "b33034107fe9441a2d8b",
+    "url": "/CSCRecipes/static/js/main.b277f0a9.chunk.js"
+  },
+  {
+    "revision": "e68e5e0c2a15eb5f8da8",
+    "url": "/CSCRecipes/static/js/runtime-main.12494854.js"
+  }
+]);
