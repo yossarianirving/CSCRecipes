efdf77f01219ab1d1f59d5a359df65f6bf7b6099
Chris Ward <acward96@gmail.com>
2019-12-08 15:37:33 -0500

    Added env to jest

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,6 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+require('dotenv').config()
 global.fetch = require('jest-fetch-mock')
 //simply json adapter
 configure({ adapter: new Adapter() });
